8ae55632235aac6ed9e012e7ab4c7e75d24e015e
Septentrion50 <erwan56lecorre@gmail.com>
2021-05-22 19:52:39 +0200

    Ajout des images d'événements et des images de profil utilisateur

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,4 +1,7 @@
 class ChargesController < ApplicationController
+
+  before_action :authenticate_user!
+
   def new
     @user = current_user
     @event = Event.find(params[:event_id])
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     resources :charges, only: [:new, :create]
     resources :images, only: [:create]
   end
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :avatars, only: [:create]
+  end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
