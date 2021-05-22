984973cac0aafdc2c5ecc1805f30e2d24426aab3
Septentrion50 <erwan56lecorre@gmail.com>
2021-05-22 15:00:18 +0200

    Mise en place des images avec active storage

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,6 @@
 class ImagesController < ApplicationController
     def create
-        @event = Event.find(params[:id])
+        @event = Event.find(params[:event_id])
         @event.image.attach(params[:image])
         redirect_to event_path(@event)
     end
